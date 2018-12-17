ea5fa95410686594bcca5f5d6ac0c84b6a21e68e
cosmindimisca <cosmin.dimisca@mindit.io>
2018-12-17 22:44:55 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,39 @@ import { RegisterComponent } from './register/register.component';
 import { Globals } from './services/globals';
 import { ProfilPageComponent } from './profil-page/profil-page.component';
 import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     ProfilPageComponent,
-    MenuComponent
+    MenuComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'profilePage',
+        component: ProfilPageComponent
+      },
+      {
+        path: 'form',
+        component: FormComponent
+      }
+    ])
   ],
   providers: [Globals],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Globals} from 'src/app/services/globals';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  currentUser = {};
+  constructor(private globals: Globals) {
+    this.currentUser = globals.currentUser;
+   }
 
   ngOnInit() {
   }
